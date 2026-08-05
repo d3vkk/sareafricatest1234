a3f97d998c1a6622f3512098c68bd2845e58b844
d3vkk <donaldkk22@yahoo.com>
2026-08-05 16:28:10 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-blue-600">
+        Sare Africa
+      </h1>
+    </>
+  );
+}
+
+export default App
